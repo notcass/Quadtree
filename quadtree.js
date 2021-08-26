339651d9c5d6a76fa4aaebffc89cec5361ad2713
unknown <cassbates@gmail.com>
2021-08-26 00:43:16 -0500

    Fixed problem with intersects function

--- quadtree.js
+++ quadtree.js
@@ -2,10 +2,11 @@ class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.size = 1; // Testing
   }
 }
 
-class RectangleThing {
+class Rectangle {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
@@ -21,11 +22,33 @@ class RectangleThing {
 
   contains(point) {
     return (
-      point.x > this.x &&
-      point.x < this.x + this.w &&
-      point.y > this.y &&
-      point.y < this.y + this.h
+      point.x >= this.x &&
+      point.x <= this.x + this.w &&
+      point.y >= this.y &&
+      point.y <= this.y + this.h
     );
+  }
+
+  intersects(range) {
+    if (
+      this.x < range.x + range.w &&
+      this.x + this.w > range.x &&
+      this.y < range.y + range.h &&
+      this.y + this.h > range.y
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  //prettier-ignore
+  intersectsBadFirstAttempt(range) {
+    // The problem with this was we were only checking if a corner of the range was inside the boundary
+    // But that doesn't work when the boundary is completely inside the range
+    let x = range.x > this.x && range.x < this.x + this.w || range.x + range.w > this.x && range.x + range.w < this.x + this.w;
+    let y = range.y > this.y && range.y < this.y + this.h || range.y + range.h > this.y && range.y + range.h < this.y + this.h;
+
+    return x || y;
   }
 }
 
@@ -37,32 +60,30 @@ class Quadtree {
     this.points = [];
     this.divided = false;
   }
-  show() {
-    this.boundary.show();
+
+  resetPointSize() {
+    this.points.forEach((p) => (p.size = 1));
     if (this.divided) {
-      this.northwest.show();
-      this.northeast.show();
-      this.southwest.show();
-      this.southeast.show();
+      this.northwest.resetPointSize();
+      this.northeast.resetPointSize();
+      this.southwest.resetPointSize();
+      this.southeast.resetPointSize();
     }
-    this.points.forEach((p) => {
-      point(p.x, p.y);
-    });
   }
 
   subdivide() {
     let { x, y, w, h } = this.boundary;
 
-    let nw = new RectangleThing(x, y, w / 2, h / 2);
+    let nw = new Rectangle(x, y, w / 2, h / 2);
     this.northwest = new Quadtree(nw, this.capacity);
 
-    let ne = new RectangleThing(x + w / 2, y, w / 2, h / 2);
+    let ne = new Rectangle(x + w / 2, y, w / 2, h / 2);
     this.northeast = new Quadtree(ne, this.capacity);
 
-    let sw = new RectangleThing(x, y + h / 2, w / 2, h / 2);
+    let sw = new Rectangle(x, y + h / 2, w / 2, h / 2);
     this.southwest = new Quadtree(sw, this.capacity);
 
-    let se = new RectangleThing(x + w / 2, y + h / 2, w / 2, h / 2);
+    let se = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
     this.southeast = new Quadtree(se, this.capacity);
     this.divided = true;
   }
@@ -72,15 +93,65 @@ class Quadtree {
 
     if (this.points.length < this.capacity) {
       this.points.push(point);
+      return true;
     } else {
       if (!this.divided) {
         this.subdivide();
       }
 
-      this.northeast.insert(point);
-      this.northwest.insert(point);
-      this.southeast.insert(point);
-      this.southwest.insert(point);
+      return (
+        this.northeast.insert(point) ||
+        this.northwest.insert(point) ||
+        this.southeast.insert(point) ||
+        this.southwest.insert(point)
+      );
+    }
+  }
+
+  query(range, found) {
+    if (!this.boundary.intersects(range)) {
+      return;
+    } else {
+      this.points.forEach((p) => {
+        if (range.contains(p)) {
+          found.push(p);
+          p.size = 5;
+        }
+      });
+
+      if (this.divided) {
+        this.northwest.query(range, found);
+        this.northeast.query(range, found);
+        this.southwest.query(range, found);
+        this.southeast.query(range, found);
+      }
+
+      return found;
+    }
+  }
+
+  showBoundaries() {
+    this.boundary.show();
+    if (this.divided) {
+      this.northwest.showBoundaries();
+      this.northeast.showBoundaries();
+      this.southwest.showBoundaries();
+      this.southeast.showBoundaries();
+    }
+  }
+
+  showPoints() {
+    stroke(255);
+    strokeWeight(1);
+    this.points.forEach((p) => {
+      circle(p.x, p.y, p.size);
+    });
+
+    if (this.divided) {
+      this.northwest.showPoints();
+      this.northeast.showPoints();
+      this.southwest.showPoints();
+      this.southeast.showPoints();
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,40 +1,82 @@
 /// <reference path="libraries/p5.global-mode.d.ts" />
 let boundary;
 let qtree;
+let range;
 let points = [];
+let boxFlag = true;
 
 function setup() {
   let canvas = createCanvas(400, 400);
   canvas.parent('sketch-holder');
   background(18);
-
-  boundary = new RectangleThing(0, 0, 400, 400);
+  reset();
+}
+function reset() {
+  boundary = new Rectangle(0, 0, 400, 400);
   qtree = new Quadtree(boundary, 4);
 
   // Lots of random points
-  // for (let i = 0; i < 500; i++) {
+  // for (let i = 0; i < 100; i++) {
   //   let p = new Point(random(width), random(height));
   //   qtree.insert(p);
-  //   points.push(p);
   // }
+
+  // Random Gaussian
+  for (let i = 0; i < 350; i++) {
+    let x = randomGaussian(width / 2, width / 8);
+    let y = randomGaussian(height / 2, height / 8);
+    let p = new Point(x, y);
+    qtree.insert(p);
+  }
+
+  // Points for testing range
+  //x: 197 - 304
+  //y: 210 - 285
+  // qtree.insert(new Point(220, 220));
+  // qtree.insert(new Point(230, 270));
+  // qtree.insert(new Point(290, 242));
+  // qtree.insert(new Point(245, 215));
+  // qtree.insert(new Point(225, 256));
+
+  // Range
+  range = new Rectangle(197, 210, 107, 75);
+  qtree.query(range, points);
+  console.log(points);
 }
 
 function draw() {
-  // Mouse control for points
-  if (mouseIsPressed) {
-    let m = new Point(mouseX, mouseY);
-    qtree.insert(m);
-  }
+  background(18);
 
-  // Random Points ever secs
-  // if (frameCount % 60 == 0) {
-  //   qtree = new Quadtree(boundary, 4);
-  //   for (let i = 0; i < 500; i++) {
-  //     let p = new Point(random(width), random(height));
-  //     qtree.insert(p);
-  //   }
+  // Mouse control for points
+  // if (mouseIsPressed) {
+  //   let m = new Point(mouseX, mouseY);
+  //   qtree.insert(m);
   // }
 
-  background(18);
-  qtree.show();
+  if (boxFlag) qtree.showBoundaries();
+  qtree.showPoints();
+
+  // Range Rect & Testing
+  stroke(0, 255, 0);
+  strokeWeight(1);
+  noFill();
+  rect(range.x, range.y, range.w, range.h);
+  // range.x = mouseX;
+  // range.y = mouseY;
+}
+
+function keyPressed() {
+  if (key === 'r') reset();
+  if (key === '1') {
+    points = [];
+    qtree.resetPointSize();
+    qtree.query(range, points);
+    console.log(`%cpoints in box: ${points.length}`, `font-size: 24px`);
+  }
+  if (key === 'b') boxFlag = !boxFlag;
+}
+
+function mousePressed() {
+  let p = new Point(mouseX, mouseY);
+  qtree.insert(p);
 }
